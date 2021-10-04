f4b26c6d3606f0482a3861b6596ba932d1a4b6db
Ghazi GASSARA <g.gassara@HLICONSULTING.local>
2021-10-04 20:58:40 +0100

    'test1287875'

--- sonar-add-project-guid.ps1
+++ sonar-add-project-guid.ps1
@@ -1,7 +1,5 @@
-# This script will add Project GUID into the .csproj.
-# That is required by Sonar Cloud or Sonar Qube.
-# Only needed for .NET Core .csproj projects.
-# .NET Framework doesn't need this as it has already a project GUID.
+
+
 
 $paths = Get-ChildItem -include *.csproj -Recurse
 foreach($pathobject in $paths) 
@@ -10,7 +8,7 @@ foreach($pathobject in $paths)
     $doc = New-Object System.Xml.XmlDocument
     $doc.Load($path)
     $child = $doc.CreateElement("ProjectGuid")
-    $child.InnerText = [guid]::NewGuid().ToString().ToUpper()
+    $child.InnerText = "{"+[guid]::NewGuid().ToString().ToUpper()+"}"
     $node = $doc.SelectSingleNode("//Project/PropertyGroup")
     $node.AppendChild($child)
     $doc.Save($path)
